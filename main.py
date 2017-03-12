efff86968b1b7e6f3cc982a8212abd7dcce9fa06
rlui001 <rlui001@ucr.edu>
2017-03-12 04:13:21 -0700

    Implemented normalize function.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Instance import Instance
+import math
 
 def nearestNeighborClassifier(data, point):
 	""" 
@@ -10,11 +11,36 @@ def nearestNeighborClassifier(data, point):
 	"""
 
 
+def normalize(data, num_features, num_instances):
+	"""
+	Normalizes the data. X = (X - mean(X)) / std(X)
+	Returns normalized instances.
+	normalized[i][j] = (instances[i][j] - mean[j-1]) / std[j-1]; because instances[i][1] = feature 1 and mean[j-1] = feature 1
+	"""
+	
+	# When using range(x to y)... starts from x stops at y - 1, so we need x to y + 1
+	# Ex) num_features = 10, then range(1 to num_features + 1) -> 1 to 10
 
+	# Get the mean for each feature, and store it in an array/list
+	# Add feature X for all instances, divide by total # instances...
+	mean = []
+	for i in range(1, num_features + 1):
+		mean.append((sum(row[i] for row in data)) / num_instances)
 
+	# Get the std for each feature, and store it in an array/list
+	# std formula -> (each point - mean)^2 / num_instances -> sqrt(variance)
+	std = []
+	for i in range(1, num_features + 1):
+		variance = sum(pow((row[i] - mean[i-1]), 2) for row in data) / num_instances
+		std.append(math.sqrt(variance))
 
+	# Return data (just modified in original data array)
+	for i in range(0, num_instances):
+		for j in range(1, num_features + 1):
+			data[i][j] = ((data[i][j] - mean[j-1]) / std[j-1])
 
-
+	return data
+	
 
 
 
@@ -58,13 +84,20 @@ def main():
 		instances[i] = [float(j) for j in data.readline().split()]
 
 	# We now have a 2D array where instance[x][0] is the classification, and instance[x][num_features] is the last feature for x
+	# x = instance id	
 	
-
-
-
-	# CONVERT/NORMALIZE DATA
-
 	# Algorithm selection
+	print 'Type the number of the algorithm you want to run.'
+	choice = int(raw_input())
+	while choice < 1 or choice > 3:
+		print 'Invalid choice, please try again.'
+		choice = int(raw_input())
+
+	print 'This dataset has ' + str(num_features) + ' features (not including the class attribute), with ' + str(num_instances) + ' instances.'
+
+	# CONVERT/NORMALIZE DATA -> begin search
+	print 'Please wait while I normalize the data... Done!'
+	normalized_instances = normalize(instances, num_features, num_instances)
 
 
 
