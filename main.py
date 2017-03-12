b0c926723e04e672a27dee27a0529d4773cfda34
rlui001 <rlui001@ucr.edu>
2017-03-12 06:57:56 -0700

    Nearest neighbor method is finished.

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
 from Instance import Instance
 import math
 
-def nearestNeighborClassifier(data, point):
+def nearestNeighborClassifier(data, point, feature_subset, num_instances):
 	""" 
 	Computes distance to training points and
 	returns the class label of the nearest training
 	point.
 	data: all the training points for this problem
-	point: the new data point that needs to be classified 
+	point: instance id of new data point that needs to be classified 
+	num_instances: total instances in data set
+	features: feature subset being used for classifier
 	"""
+	# Initialize variables for whole scope of function
+	nearestNeighbor = 0
+	shortest_distance = float('inf')
+
+	# Loop over all instances
+	for i in range(num_instances):
+		# If the instance == the point, ignore/do nothing
+		if point == i:
+			pass
+		# Else, find nearest neighbor and classify the point
+		else:
+			# nearestNeighbor keeps track of the nearest neighbor
+			# shortest_distance determines which is nearest neighbor
+			distance = 0
+			# Get distance, compare and update nearestNeighbor
+			# feature_subset: subset of all features. ex) [1,2,4] -> features 1, 2 and 4
+			for j in range(len(feature_subset)):
+				distance = distance + pow((data[i][feature_subset[j]] - data[point][feature_subset[j]]), 2)
+
+			distance = math.sqrt(distance)
+
+			if distance < shortest_distance:
+				nearestNeighbor = i 
+				shortest_distance = distance
+
+	return nearestNeighbor
+
+
+def oneOutValidator(data, feature_subset, num_instances, num_features):
+	"""
+	Performs the one-out validation algorithm and returns the accuracy
+	"""
+	# FIX: Parameters may need to be changed depending on future fixes for algorithm
+
 
 
 def normalize(data, num_features, num_instances):
@@ -17,7 +53,7 @@ def normalize(data, num_features, num_instances):
 	Returns normalized instances.
 	normalized[i][j] = (instances[i][j] - mean[j-1]) / std[j-1]; because instances[i][1] = feature 1 and mean[j-1] = feature 1
 	"""
-	
+
 	# When using range(x to y)... starts from x stops at y - 1, so we need x to y + 1
 	# Ex) num_features = 10, then range(1 to num_features + 1) -> 1 to 10
 
@@ -40,7 +76,7 @@ def normalize(data, num_features, num_instances):
 			data[i][j] = ((data[i][j] - mean[j-1]) / std[j-1])
 
 	return data
-	
+
 
 
 
@@ -98,6 +134,20 @@ def main():
 	# CONVERT/NORMALIZE DATA -> begin search
 	print 'Please wait while I normalize the data... Done!'
 	normalized_instances = normalize(instances, num_features, num_instances)
+
+	# Run nearest neighbor + one out validation + ALL features, print results
+	all_features = []
+	for i in range(1, num_features + 1):
+		all_features.append(i)
+
+	#accuracy = oneOutValidator(normalized_instances, all_features, num_instances, num_features)
+	#print 'Running nearest neighbor with all ', num_features, ' features, using "leaving-one-out" evaluation, I get an accuracy of ', accuracy, '.'
+
+
+	# Run the algorithm chosen with feature subsets
+
+	# TO FIX: Add algorithm to make the subsets in the chosen algorithms
+
 
 
 
