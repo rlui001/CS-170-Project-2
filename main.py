fc6c589c994e19fce80097d46b12d600800ff7cb
rlui001 <rlui001@ucr.edu>
2017-03-12 01:14:51 -0800

    Implemented IO functionality

--- main.py
+++ main.py
@@ -23,8 +23,50 @@ def nearestNeighborClassifier(data, point):
 
 def main():
 	print 'Welcome to Ronson Lui\'s Feature Selection Algorithm.'
-	print 'Type in the name of the file to test: '
-	file = raw_input()
+	file = raw_input('Type in the name of the file to test: ')
+
+	# First column is the class, value always 1 or 2.
+	# Other columns = features, maximum up to 64
+	# 1 x x x x x x x x x ...
+	# 2 x x x x x x x x x ...
+	# Number of instances = total lines in file
+	# Number features = total in one line - 1, bc first is classification
+	# Max instances = 2048
+
+	# Store data from file
+	# Open file, error exception
+	try:
+		data = open(file, 'r')
+	except:
+		raise IOError('The file '+ file +' does not exist. Exiting program.')
+
+	# Read in first line to see # features 
+	firstLine = data.readline()
+
+	num_features = len(firstLine.split()) - 1
+
+	# Read in all lines on file to get # instances
+	data.seek(0)
+	num_instances = sum(1 for line in data)
+
+	# Use seek(0) to reset cursor to start of file
+	data.seek(0)
+
+	# Store data into variable/array
+	instances = [[] for i in range(num_instances)]
+	for i in range(num_instances):
+		instances[i] = [float(j) for j in data.readline().split()]
+
+	# We now have a 2D array where instance[x][0] is the classification, and instance[x][num_features] is the last feature for x
+	
+
+
+
+	# CONVERT/NORMALIZE DATA
+
+	# Algorithm selection
+
+
 
 
 
@@ -42,4 +84,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-# Grab data first and store in Instance object
